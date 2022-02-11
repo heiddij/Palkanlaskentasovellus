4d972d78633d871d01664232065697dd0f53433f
Heidi Joutsijoki <heidi20151@student.hamk.fi>
2022-02-11 20:19:42 +0200

    Lisätty käyttöliittymän ensimmäiset toiminnot

--- PalkanlaskentaSovellus/PalkanlaskentaSovellus/Program.cs
+++ PalkanlaskentaSovellus/PalkanlaskentaSovellus/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Palkanlaskenta sovellus
 
 // Ohjelman pääluokka, ns. pääohjelma, josta kaikki toiminnallisuus alkaa
 
@@ -13,6 +11,33 @@ class MainClass
 
     public static void Main(string[] args)
     {
+
+        Console.WriteLine("Tervetuloa Palkanlasku-sovellukseen. Valitse rooli:");
+        Console.WriteLine("[1] Henkilöstöpäällikkö");
+        Console.WriteLine("[2] Palkanlaskija");
+        int rooli = int.Parse(Console.ReadLine());
+
+        if (rooli != 1 || rooli != 2)
+        {
+            Console.WriteLine("Väärä syöte! Valitse joko 1 tai 2");
+            rooli = int.Parse(Console.ReadLine());
+        }
+
+        if (rooli == 1)
+        {
+            Console.WriteLine("Henkilöstöpäällikkö, valitse toiminto:");
+            Console.WriteLine("[1] Lista työntekijöistä");
+            Console.WriteLine("[2] Lisää työntekijä");
+            Console.WriteLine("[3] Poista työntekijä");
+            int toiminto1 = int.Parse(Console.ReadLine());
+        }
+
+        if (rooli == 2)
+        {
+            Console.WriteLine("Palkanlaskija, valitse toiminto:");
+            Console.WriteLine("[1] Lista työntekijöistä");
+            int toiminto2 = int.Parse(Console.ReadLine());
+        }
     }
 
 }
