3cfaf516e8efc09412c7f0e5b6ffde47740f996c
Heidi Joutsijoki <heidi20151@student.hamk.fi>
2022-02-11 11:11:39 +0200

    Sovelluksen pohja ja gitignore

---
+++ PalkanlaskentaSovellus/PalkanlaskentaSovellus/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Palkanlaskenta sovellus
+
+// Ohjelman pääluokka, ns. pääohjelma, josta kaikki toiminnallisuus alkaa
+
+class MainClass
+{
+    // Main metodia kutsutaan kun ohjelma käynnistyy. Tämä on ensimmäinen metodi mitä ohjelma kutsuu ja mistä
+    // kaikki alkaa. 
+    //
+    // "string[] args" tarkoittaa parametreja, jos niitä on annettu ohjelman alkaessa esim. komentokehotteesta
+
+    public static void Main(string[] args)
+    {
+    }
+
+}
