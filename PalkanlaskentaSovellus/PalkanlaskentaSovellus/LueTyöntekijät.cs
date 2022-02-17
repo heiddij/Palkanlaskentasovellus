6b34640c1537eae0e42810ec2af238ec99cb6468
juhonevalainen <97619711+juhonevalainen@users.noreply.github.com>
2022-02-17 19:22:19 +0200

    Metodi työntekijän hakemiseksi tiedostosta.

--- PalkanlaskentaSovellus/PalkanlaskentaSovellus/LueTyöntekijät.cs
+++ PalkanlaskentaSovellus/PalkanlaskentaSovellus/LueTyöntekijät.cs
@@ -23,4 +23,29 @@ internal class LueTyöntekijät
         }
         reader.Close();
     }
+
+    // Haetaan työntekijälistasta sukunimi.
+    // Jos löytyy, niin palauttaa rivin mistä työntekijä löytyy int arvolla. Muuten palautus 0.
+    public int HaeTyontekija(string hakusana)
+    {
+        int palautus = 0;
+        int rivinLaskija = 1;
+        if (hakusana.All(Char.IsLetter) == true) // Tarkistetaan että haetaan vain kirjaimilla.
+        {
+            StreamReader reader = File.OpenText("../../../../työntekijät.txt");
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                if (line.Contains(hakusana))
+                {
+                    palautus = rivinLaskija;
+                    break;
+                }
+                line = reader.ReadLine();
+                rivinLaskija++;
+            }
+            reader.Close();
+        }
+        return palautus;
+    }
 }
